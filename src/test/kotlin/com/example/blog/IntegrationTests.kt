8e8a8cec93d65fe41b9bafc14a6cb785c4b65696
Jonathan Alberth Quispe Fuentes <qf.jonathan@gmail.com>
2022-10-23 22:33:06 -0500

    fix article tests

--- src/test/kotlin/com/example/blog/IntegrationTests.kt
+++ src/test/kotlin/com/example/blog/IntegrationTests.kt
@@ -28,8 +28,8 @@ class IntegrationTests(@Autowired val restTemplate: TestRestTemplate) {
     @Test
     fun `Assert article page title content and status code`() {
         println(">> Assert article page title, content and status code")
-        val title = "Reaction Aluminium has loaded"
-        val entity = restTemplate.getForEntity<String>("/artitle/${title.toSlug()}")
+        val title = "Reactor Aluminium has loaded"
+        val entity = restTemplate.getForEntity<String>("/article/${title.toSlug()}")
         assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
         assertThat(entity.body).contains(title, "Lorem ipsum", "dolor sit amet")
     }
